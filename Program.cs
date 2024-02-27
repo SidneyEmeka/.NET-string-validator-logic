03680bf75522cbf627081289219dc03dba2e8088
Sidswipe <sidswipexyz@gmail.com>
2024-02-27 11:48:56 +0100

    string validator; nullable string type

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string ? readUser; //initiate a nullable string to collect user input
+string roleName = ""; //to store user input
+bool validate = false; //boolean to validate iteration
+
+Console.WriteLine($"Enter your role name (Administrator, Manager, or User)"); //To prompt user for input
+
+do{
+    readUser = Console.ReadLine(); //collects input and 
+    if (readUser != null)
+    {
+        roleName = readUser.Trim().ToLower(); //stores in rolename
+    }
+    if (roleName.Contains("administrator") || roleName.Contains("manager") || roleName.Contains("user")) //conditional
+    {
+        validate = true;
+   }
+   else {
+    Console.WriteLine($"The role name that you entered, {roleName} is not valid. Enter your role name (Administrator, Manager, or User)");
+   }
+} while (validate == false);
+Console.WriteLine($"Welcome {roleName.ToUpper()}");
---
+++ README.md
@@ -0,0 +1,10 @@
+# String Validator
+Inside the iteration block:
+
+The Logic uses a Console.ReadLine() statement to obtain input from the user.<br>
+The Logic ensures that the value entered matches one of the three role options.<br>
+The Logic uses the Trim() method on the input value to ignore leading and trailing space characters.
+Your solution should use the ToLower() method on the input value to ignore case.
+If the value entered isn't a match for one of the role options, the code uses a Console.WriteLine() statement to prompt the user for a valid entry.
+
+## Nullable string type
